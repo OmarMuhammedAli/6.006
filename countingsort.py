95429bf62367fe30b496b5576bb339b3bbb8b22f
Omar <omarmuhammed1998.om@gmail.com>
2021-10-01 20:46:20 +0200

    add comments for clarity

--- countingsort.py
+++ countingsort.py
@@ -1,14 +1,14 @@
 def counting_sort(a):
-    u = max(a) + 1
-    c = [0] * u
+    u = max(a) + 1 # max element in the array that dictates the size of the output array
+    c = [0] * u # auxiliary array for counting the number of occurrences of each element
     for i in a:
-        c[i] += 1
+        c[i] += 1 # count the number of occurrences of each element
     for i in range(1, u):
-        c[i] += c[i - 1]
-    b = [0] * len(a)
+        c[i] += c[i - 1] # add the number of occurrences of each element to the previous element
+    b = [0] * len(a) # output array
     for i in a[-1::-1]:
-        b[c[i] - 1] = i
-        c[i] -= 1
+        b[c[i] - 1] = i # put the element in the correct position in the output array
+        c[i] -= 1    # update the number of occurrences of the element
 
     return b
 
