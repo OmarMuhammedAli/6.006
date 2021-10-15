8c3bb6dc7c8b95ec1d8e6e3bdc263edfb682c6c4
Omar <omarmuhammed1998.om@gmail.com>
2021-10-15 04:21:34 +0200

    Implement top level binary tree data structure

--- week4/BT.py
+++ week4/BT.py
@@ -1,9 +1,9 @@
 class Binary_Node:
-    def __init__(A, data, parent=None):
+    def __init__(A, data):
         A.data = data
         A.left = None
         A.right = None
-        A.parent = parent
+        A.parent = None
 
     # Traversal methods
     def subtree_itr(A, order='inorder'):
@@ -93,22 +93,55 @@ class Binary_Node:
         for i in A.subtree_itr(order):
             print(i.data, end=' ')
         print()
+
+class Binary_Tree:
+    def __init__(T, Node_Type=Binary_Node):
+        T.root = None
+        T.Node_Type = Node_Type
+        T.size = 0
+    
+    def __len__(T):
+        return T.size
+    
+    def __iter__(T):
+        if T.root:
+            yield from T.root.subtree_itr()
+    
+    def build(self, data):
+        A = [item for item in data]
+        def build_subtree(A, i, j):
+            c = (i+j)//2
+            root = self.Node_Type(A[c])
+            if i < c:
+                root.left = build_subtree(A, i, c-1)
+                root.left.parent = root
+            if c < j:
+                root.right = build_subtree(A, c+1, j)
+                root.right.parent = root
+            return root
+        self.root = build_subtree(A, 0, len(A)-1)
+        self.size = len(A)
+    
 def main():
-    root = Binary_Node(10)
-    root.left = Binary_Node(6, parent=root)
-    root.right = Binary_Node(14, parent=root)
-    root.left.left = Binary_Node(4, parent=root.left)
-    root.left.right = Binary_Node(8, parent=root.left)
-    root.right.left = Binary_Node(12, parent=root.right)
-    root.right.right = Binary_Node(16, parent=root.right)
-    root.print_subtree()
-    print(root.right.left.subtree_delete().data)
-    print('\nafter deletion: ')
-    root.print_subtree()
+    # root = Binary_Node(10)
+    # root.left = Binary_Node(6, parent=root)
+    # root.right = Binary_Node(14, parent=root)
+    # root.left.left = Binary_Node(4, parent=root.left)
+    # root.left.right = Binary_Node(8, parent=root.left)
+    # root.right.left = Binary_Node(12, parent=root.right)
+    # root.right.right = Binary_Node(16, parent=root.right)
+    # root.print_subtree()
+    # print(root.right.left.subtree_delete().data)
+    # print('\nafter deletion: ')
+    # root.print_subtree()
     
     # print(f'\nmin: {root.subtree_min().data}')
     # print(f'max: {root.subtree_max().data}')
     # print(f'successor: {root.successor().data}')
     # print(f'predecessor: {root.predecessor().data}')
+    tree = Binary_Tree()
+    tree.build([1,2,3,4,5,6,7,8,9,10])
+    for i in tree:
+        print(i.data, end=' ')
 if __name__ == '__main__':
     main()
